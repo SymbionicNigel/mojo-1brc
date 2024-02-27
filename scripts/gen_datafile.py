628dfebcd888c6a086eae084a645994a72f226d1
Nigel Krajcer <nphilmont2010@gmail.com>
2024-02-27 01:27:05 -0500

    feat: first iteration for script to create data file

--- scripts/gen_datafile.py
+++ scripts/gen_datafile.py
@@ -1,6 +1,12 @@
 from dataclasses import dataclass
 from enum import auto, StrEnum
-import random, string
+import random, string, os
+
+
+class ConfigNames(StrEnum):
+    KILO = auto()
+    TENTH = auto()
+    FULL = auto()
 
 
 @dataclass
@@ -8,6 +14,20 @@ class TestFileConfiguration:
     total_rows: int
     unique_locaions: int
     range: tuple[float, float]
+    configName: ConfigNames
+
+
+TEMP_RANGE = (-99.9, 99.9)
+
+CONFIGS: dict[ConfigNames, TestFileConfiguration] = {
+    ConfigNames.KILO: TestFileConfiguration(1_000, 50, TEMP_RANGE, ConfigNames.KILO),
+    ConfigNames.TENTH: TestFileConfiguration(
+        1_000_000, 10_00, TEMP_RANGE, ConfigNames.TENTH
+    ),
+    ConfigNames.FULL: TestFileConfiguration(
+        1_000_000_000, 10_000, TEMP_RANGE, ConfigNames.FULL
+    ),
+}
 
 
 class TestGenerator:
@@ -17,6 +37,10 @@ class TestGenerator:
 
     def __init__(self, config: TestFileConfiguration) -> None:
         self.config = config
+        self.generateKeys()
+        self.generateAllRows()
+        self.shuffleRows()
+        self.writeToFile("test_data")
 
     def _generateLocationName(self, stringLength: int):
         return "".join(random.choices(string.ascii_letters, k=stringLength))
@@ -37,36 +61,28 @@ class TestGenerator:
         assert len(self.keys) == self.config.unique_locaions
 
     def generateRow(self):
-        pass
+        return f"{random.choice(list(self.keys))};{round(random.uniform(self.config.range[0], self.config.range[1]), 1)}\n"
+
+    def generateAllRows(self):
+        self.rows = [self.generateRow() for _ in range(self.config.total_rows)]
+        assert len(self.rows) == self.config.total_rows
 
     def shuffleRows(self):
-        pass
+        random.shuffle(self.rows)
 
     def writeToFile(self, filepath: str):
-        with open(filepath, "w") as file_obj:
+        parent_filepath = "./.test_resources"
+        if not os.path.exists(parent_filepath):
+            os.mkdir(parent_filepath)
+        assert os.path.exists(parent_filepath)
+        with open(
+            f"{parent_filepath}/{filepath}_{self.config.configName}.txt", "w+"
+        ) as file_obj:
             file_obj.writelines(self.rows)
 
 
-class ConfigNames(StrEnum):
-    KILO = auto()
-    TENTH = auto()
-    FULL = auto()
-
-
-TEMP_RANGE = (-100, 100)
-
-CONFIGS: dict[ConfigNames, TestFileConfiguration] = {
-    ConfigNames.KILO: TestFileConfiguration(1_000, 50, TEMP_RANGE),
-    ConfigNames.TENTH: TestFileConfiguration(1_000_000, 10_00, TEMP_RANGE),
-    ConfigNames.FULL: TestFileConfiguration(1_000_000_000, 10_000, TEMP_RANGE),
-}
-
-
 def main():
-    config = CONFIGS[ConfigNames.KILO]
-
-    generator = TestGenerator(config=config)
-    generator.generateKeys()
+    [TestGenerator(config=config) for _, config in CONFIGS.items()]
 
 
 if __name__ == "__main__":
