497fc5eb74271b6d99962bbf15db8bb68d3465e0
Nigel Krajcer <nphilmont2010@gmail.com>
2024-05-23 17:37:20 -0400

    feat: base implementation with bugs

--- scripts/challenge.py
+++ scripts/challenge.py
@@ -1,10 +1,13 @@
 from argparse import ArgumentParser
 from cProfile import Profile
+from multiprocessing import Manager, Pool
 import os, pstats, pathlib
 from pstats import SortKey
 import time
 from typing import IO, Any, Callable
 from functools import wraps
+
+FILEPATH = "./.test_resources/test_data_kilo.txt"
 
 
 def timingTool(
@@ -71,9 +74,123 @@ def timingTool(
     return timingTool
 
 
-@timingTool(profile=True, storeInRecords=True, saveStats=True)
+class LocationData:
+    __slots__ = "min_temp", "max_temp", "count", "sum_temp", "loc_str"
+    min_temp: float
+    max_temp: float
+    count: int
+    sum_temp: float
+    loc_str: str
+
+
+class Challenge:
+    manager = Manager()
+
+    def __init__(self) -> None:
+        self.data = self.manager.dict()
+
+    def readFile(self):
+        def callback(objs: Any):
+            print("callback", len(objs))
+            for idx, obj in enumerate(objs):
+                try:
+                    self.mergeData(obj)
+                except Exception as e:
+                    print(e)
+            print(len(self.data.keys()))
+
+        def error_callback(obj: BaseException):
+            print("error_callback", obj)
+
+        def fileIterator():
+            with open(FILEPATH) as dataFile:
+                while lineGroup := dataFile.readlines(4096):
+                    yield lineGroup
+
+        fileIter = fileIterator()
+        x = None
+        with Pool() as p:
+            x = p.map_async(
+                self.readFileIter,
+                fileIter,
+                chunksize=10,
+                callback=callback,
+                error_callback=error_callback,
+            )
+
+            x.wait()
+
+    def readFileIter(self, lineGroups: list[str]):
+        workingData = {}
+        print(len(lineGroups))
+        for line in lineGroups:
+            location, temp_str = line.removesuffix("\n").split(";")
+            self.upsert_row(location.capitalize(), float(temp_str), workingData)
+        return workingData
+
+    def mergeData(self, itemToMerge):
+        print(sum(x["count"] for x in itemToMerge.values()))
+        for location, partial in itemToMerge.items():
+            if location not in self.data:
+                self.data[location] = partial
+            else:
+                # print("else")
+                if self.data[location]["max_temp"] < partial["max_temp"]:
+                    self.data[location]["max_temp"] = partial["max_temp"]
+                if self.data[location]["min_temp"] > partial["min_temp"]:
+                    self.data[location]["min_temp"] = partial["min_temp"]
+                # with workingData
+                self.data[location]["count"] += partial["count"]
+                self.data[location]["sum_temp"] += partial["sum_temp"]
+
+    def upsert_row(self, location: str, temperature: float, workingData: Any):
+        if location not in workingData:
+            workingData[location] = {
+                "loc_str": location,
+                "max_temp": temperature,
+                "min_temp": temperature,
+                "sum_temp": temperature,
+                "count": 1,
+            }
+        else:
+            tempData = workingData[location]
+            if workingData[location]["max_temp"] < temperature:
+                tempData["max_temp"] = temperature
+            if workingData[location]["min_temp"] > temperature:
+                tempData["min_temp"] = temperature
+            tempData["count"] += 1
+            tempData["sum_temp"] += temperature
+            workingData[location] = tempData
+
+    def sort_and_print(self):
+        for location in sorted(self.data.keys()):
+            self.print_row(
+                location=self.data[location]["loc_str"],
+                min_temp=self.data[location]["min_temp"],
+                max_temp=self.data[location]["max_temp"],
+                average=(
+                    self.data[location]["sum_temp"] / self.data[location]["count"]
+                ),
+                count=self.data[location]["count"],
+            )
+        print(sum(x["count"] for x in self.data.values()))
+
+    def print_row(
+        self,
+        location: str,
+        min_temp: float,
+        max_temp: float,
+        average: float,
+        count: int,
+    ):
+        print(f"{location},{min_temp},{max_temp},{average},{count}")
+
+
+@timingTool(profile=False, storeInRecords=False, saveStats=False)
 def runChallenge():
-    time.sleep(1)
+    x = Challenge()
+    x.readFile()
+    x.sort_and_print()
     return
 
 
